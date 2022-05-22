90e042e3ebc72390fd9542e7c37ebc7b0a701ffe
Sébastien Deleuze <sdeleuze@vmware.com>
2022-05-22 09:40:14 +0200

    Switch to stable Rust

--- run_wasi.sh
+++ run_wasi.sh
@@ -1,1 +1,1 @@
-CARGO_TARGET_WASM32_WASI_RUNNER="wasmtime run --tcplisten 127.0.0.1:3000 --env LISTEN_FDS=1" cargo +nightly run --release --target wasm32-wasi  --example hello_wasi
+CARGO_TARGET_WASM32_WASI_RUNNER="wasmtime run --tcplisten 127.0.0.1:3000 --env LISTEN_FDS=1" cargo run --release --target wasm32-wasi --example hello_wasi
